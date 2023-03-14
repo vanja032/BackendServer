14089dc95fa27f923d7206212f3790d80beeb49d
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2023-03-14 09:46:26 +0100

    Update api.js

--- game_api_server/api.js
+++ game_api_server/api.js
@@ -20,6 +20,7 @@ api.use((request, result, next) => {
         res.header("Access-Control-Allow-Methods", "POST, GET, PUT");
         return res.status(200).json({});
     }
+    next(); // pass control to the next middleware function
 });
 */
 
