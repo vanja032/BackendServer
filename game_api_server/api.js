9bdacf934efa04d80e92fc5d107eef39a58f7277
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2023-03-14 09:35:52 +0100

    Update api.js

--- game_api_server/api.js
+++ game_api_server/api.js
@@ -11,6 +11,18 @@ api.use(parser.urlencoded({extended: false})); // 'extended: false' means that o
 //instead of 'extended: true' that means that json supports more values except only strings
 api.use(parser.json());
 
+/*
+api.use((request, result, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested_With, Content-Type, Accept, Authorization");
+    
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "POST, GET, PUT");
+        return res.status(200).json({});
+    }
+});
+*/
+
 api.use(cors({
     origin: '*'
 }));
